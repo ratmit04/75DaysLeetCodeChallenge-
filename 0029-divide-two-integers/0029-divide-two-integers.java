d4816ea73ca79e4f1c56cdf45e09d013a6b13d4c
ratmit04 <ratnesh.mittal04@gmail.com>
2026-03-11 18:27:25 +0530

    Time: 1 ms (90.67%), Space: 42.9 MB (27.32%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,10 +1,10 @@
 class Solution {
     public int divide(int dividend, int divisor) {
         int quotient=dividend/divisor;
-        if(quotient<((int)(Math.pow(-2,31))))
-            return ((int)(Math.pow(-2,31)));
-        if(quotient>((int)((Math.pow(2,31))-1)))
-            return ((int)((Math.pow(2,31))-1));
+        if(quotient<Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        if(quotient>Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
         if(quotient==Integer.MIN_VALUE&&divisor==-1)
             return Integer.MAX_VALUE;
         return quotient;
